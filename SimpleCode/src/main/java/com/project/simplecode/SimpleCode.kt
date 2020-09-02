93e37d254af9e2bc1954e6a7a7963367a1a982f6
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-02 15:54:19 +0900

    [UPDATE] gradle & code

---
+++ SimpleCode/src/main/java/com/project/simplecode/SimpleCode.kt
@@ -0,0 +1,29 @@
+package com.project.simplecode
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+class SimpleCode {
+
+    fun AppCompatActivity.intentActivity(activity: Class<*>) {
+        val intent = Intent(this, activity)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+    }
+
+    fun AppCompatActivity.noFinishIntentActivity(activity: Class<*>) {
+        val intent = Intent(this, activity)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+    }
+
+    fun AppCompatActivity.shortToast(text : String) {
+        android.widget.Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_SHORT).show()
+    }
+
+    fun AppCompatActivity.longToast(text : String) {
+        android.widget.Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_LONG).show()
+    }
+
+}

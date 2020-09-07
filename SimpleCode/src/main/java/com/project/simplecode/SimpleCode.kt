aa214502cc3fb04b66126d9c95f49f6b73ca10cd
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-07 11:36:43 +0900

    [UPDATE] 1.1.1 version

--- SimpleCode/src/main/java/com/project/simplecode/SimpleCode.kt
+++ SimpleCode/src/main/java/com/project/simplecode/SimpleCode.kt
@@ -1,25 +1,28 @@
 package com.project.simplecode
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-    fun AppCompatActivity.intentActivity(activity: Class<*>) {
-        val intent = Intent(this, activity)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
-        finish()
-    }
+fun AppCompatActivity.simIntent(activity: Class<*>) {
+    val intent = Intent(this, activity)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+    startActivity(intent)
+    finish()
+}
 
-    fun AppCompatActivity.noFinishIntentActivity(activity: Class<*>) {
-        val intent = Intent(this, activity)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
-    }
+fun AppCompatActivity.simIntentNoFinish(activity: Class<*>) {
+    val intent = Intent(this, activity)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+    startActivity(intent)
+}
 
-    fun AppCompatActivity.shortToast(text : String) {
-        android.widget.Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_SHORT).show()
-    }
+fun AppCompatActivity.simToastShort(text: String) {
+    Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_SHORT).show()
+}
 
-    fun AppCompatActivity.longToast(text : String) {
-        android.widget.Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_LONG).show()
-    }
+fun AppCompatActivity.simToastLong(text: String) {
+    Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_LONG).show()
+}
+
+
--- app/src/main/java/com/project/school_project/MainActivity.kt
+++ app/src/main/java/com/project/school_project/MainActivity.kt
@@ -2,14 +2,16 @@ package com.project.school_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.project.simplecode.SimpleCode
+import android.widget.Toast
+import com.project.simplecode.intentActivity
+import com.project.simplecode.longToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+
     }
 
 

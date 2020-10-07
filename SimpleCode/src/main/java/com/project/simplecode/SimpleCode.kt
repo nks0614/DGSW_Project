ab7ad812af065b1852293b6dfcd51166e15b88ee
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-07 14:51:12 +0900

    [UPDATE] 1.6.0 version

--- SimpleCode/src/main/java/com/project/simplecode/SimpleCode.kt
+++ SimpleCode/src/main/java/com/project/simplecode/SimpleCode.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import java.lang.NumberFormatException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -33,12 +32,22 @@ fun AppCompatActivity.spaToastLong(text: String) {
 
 //Fragment
 fun Fragment.spfIntent(moveActivity: Class<*>) {
-    startActivity(Intent(context!!.applicationContext, moveActivity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    startActivity(
+        Intent(
+            context!!.applicationContext,
+            moveActivity
+        ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    )
     activity!!.finish()
 }
 
 fun Fragment.spfIntentNoFin(moveActivity: Class<*>) {
-    startActivity(Intent(context!!.applicationContext, moveActivity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    startActivity(
+        Intent(
+            context!!.applicationContext,
+            moveActivity
+        ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    )
 }
 
 fun Fragment.spfToastShort(message: String?) {
@@ -51,32 +60,97 @@ fun Fragment.spfToastLong(message: String?) {
 
 
 //Function
-fun spDateFormat(format : String, num : Int) : String {
-    var cal : Calendar = GregorianCalendar()
+fun spDateFormat(format: String, num: Int): String {
+    var cal: Calendar = GregorianCalendar()
     cal.add(Calendar.DATE, num)
 
-    var date : Date = cal.time
-    var dateFormat : SimpleDateFormat = SimpleDateFormat(format)
-    var getTime : String = dateFormat.format(date)
+    var date: Date = cal.time
+    var dateFormat: SimpleDateFormat = SimpleDateFormat(format)
+    var getTime: String = dateFormat.format(date)
 
     return getTime
 }
 
-fun spStringDigit(text : String) : Boolean {
-    try{
-        var i : Int = text.toInt()
-    }catch (e : NumberFormatException){
+fun spStringDigit(text: String): Boolean {
+    try {
+        var i: Int = text.toInt()
+    } catch (e: NumberFormatException) {
         return false
     }
     return true
 }
 
-fun spStringConcat(vararg text:String) : String{
-    var result : String = ""
-    for(s in text){
+fun spStringConcat(vararg text: String): String {
+    var result: String = ""
+    for (s in text) {
         result += s
     }
     return result
+}
+
+fun spRandom(start: Int, end: Int): Int {
+    val r = Random()
+    return r.nextInt((end + 1) - start) + start
+}
+
+fun spAverage(vararg num: Double): Double {
+    var result : Double = 0.0;
+    for (i in num) {
+        result += i
+    }
+
+    result /= num.size
+
+    return result
+}
+
+fun spAverage(vararg num: Int): Double {
+    var result : Double = 0.0;
+    for (i in num) {
+        result += i
+    }
+
+    result /= num.size
+
+    return result
+}
+
+fun spStringSimilarity(s1: String, s2: String): Double {
+    val longStrLen = s1.length + 1
+    val shortStrLen = s2.length + 1
+
+    var cost = IntArray(longStrLen)
+    var newcost = IntArray(longStrLen)
+
+    for (i in 0 until longStrLen) {
+        cost[i] = i
+    }
+
+
+    for (j in 1 until shortStrLen) {
+
+        newcost[0] = j
+        for (i in 1 until longStrLen) {
+
+            var match = 0
+            if (s1[i - 1] != s2[j - 1]) {
+                match = 1
+            }
+
+
+            val replace = cost[i - 1] + match
+            val insert = cost[i] + 1
+            val delete = newcost[i - 1] + 1
+
+            newcost[i] = Math.min(Math.min(insert, delete), replace)
+        }
+
+        val temp = cost
+        cost = newcost
+        newcost = temp
+    }
+
+    return (s2.length - cost[longStrLen - 1]) / s2.length.toDouble() * 100
 }
 
 
